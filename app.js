ef8e211470bc8200e83399d586afc0bdec275e27
PhiliciaWatson <philiciawatson7@gmail.com>
2021-04-14 22:31:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var Var1 = 65;
+var Var2 = 103;
+var Var3 = 99;
+var Var4 = 87;
+var Var5 = 45;
+var Var6 = 42;
+var Var7 = 12;
+var Var8 = 77;
+var Var9 = 16;
+var Var10 = 2;
+
+function Adding(num1,num2){
+
+    return num1 + num2;
+}
+
+console.log(Adding(Var1,Var2));
+console.log(Adding(Var1,Var2));
+
+
+
+// Subtraction
+function Sub(num1,num2){
+
+    return num1 - num2;
+}
+
+console.log(Sub(Var3,Var4));
+console.log(Sub(Var3,Var4));
+
+
+// Multiply
+function Multiply(num1,num2){
+
+    return num1 * num2;
+}
+
+console.log(Multiply(Var5,Var6));
+console.log(Multiply(Var5,Var6));
+
+
+// Divison
+function Divison(num1,num2){
+
+   return num1 / num2;
+
+}
+
+console.log(Divison(Var7,Var8));
+console.log(Divison(Var7,Var8));
+
+
+// Increments
+function Salty(num1){
+
+    return ++num1;
+
+}
+console.log(Salty(Var9));
+console.log(Salty(Var9));
+
+// Dercrement
+
+function Cakes(num1){
+
+    return --num1;
+
+}
+console.log(Cakes(Var10));
+console.log(Cakes(Var10));
